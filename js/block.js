516b23d70d701e13840a82742a4d821e9490fe75
Jahid Hasan <jahidhasanmisir@gmail.com>
2024-09-29 00:54:36 +0600

    made navbar sticky

--- js/block.js
+++ js/block.js
@@ -1,6 +1,7 @@
 document.getElementById('donation-btn').addEventListener('click', function(){
     document.getElementById('donation-block').classList.remove('hidden');
     document.getElementById('history-block').classList.add('hidden');
+    document.getElementById('footer').classList.remove('hidden');
 
     document.getElementById('donation-btn').classList.add('bg-blue-300');
     document.getElementById('history-btn').classList.remove('bg-blue-300');
@@ -12,4 +13,5 @@ document.getElementById('history-btn').addEventListener('click', function(){
 
     document.getElementById('donation-btn').classList.remove('bg-blue-300');
     document.getElementById('history-btn').classList.add('bg-blue-300');
+    document.getElementById('footer').classList.add('hidden');
 })
